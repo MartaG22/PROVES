00fcc17d82c125bf5c6034f26748266b41272fa8
Marta <martaxgarijo@gmail.com>
2022-12-12 10:27:45 +0100

    ordenant carpetes

--- Entrega_3.2/2-Decorator/producte.js
+++
@@ -1,9 +0,0 @@
-class Producte {
-    constructor(nomProducte, preu, divisa){
-        this.nomProducte = nomProducte;
-        this.preu = preu;
-        this.divisa = divisa;
-    }
-};
-
-module.exports = Producte;
